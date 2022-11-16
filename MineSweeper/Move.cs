388b352e5a011fdf9f4eeec4962703a9741f7a84
Acidus <acidus99@users.noreply.github.com>
2022-11-16 17:17:33 -0500

    be explicit about click vs placing a flag

--- MineSweeper/Move.cs
+++ MineSweeper/Move.cs
@@ -3,6 +3,7 @@ namespace MineSweeper
 {
     public class Move
     {
+        //if its not a click, its placing a flag
         public bool IsClick { get; set; } = true;
         public byte Row { get; set; }
         public byte Column { get; set; }
